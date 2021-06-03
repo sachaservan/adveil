9a8133d03d353baa0a37285d3e82b8fdbb30077d
Sacha Servan-Schreiber <sachaservanschreiber@gmail.com>
2021-06-03 07:50:36 -0400

    One table per core in experiment

--- cmd/server/scripts/experiment2.sh
+++ cmd/server/scripts/experiment2.sh
@@ -15,6 +15,6 @@ do
         # "$@" contains all parameters that are passed to the script (and do not change between experiments)
         # note: --numprocs should be small because it only affects the table-level parallelization 
         # all table queries are still executed in parallel with one-another (with each query parallelized by --numprocs). 
-        bash scripts/run.sh --numads $dbsize --size 1 --numfeatures 100 --numtables $numtables --bucketsize 1 --numprocs 3 "$@"
+        bash scripts/run.sh --numads $dbsize --size 1 --numfeatures 100 --numtables $numtables --bucketsize 1 --numprocs 1 "$@"
    done
 done
