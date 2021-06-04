47db435de34ae79e4b3cae39b8528055c64cce7e
Sacha Servan-Schreiber <sachaservanschreiber@gmail.com>
2021-06-04 11:41:18 -0400

    Tweak accuracy params

--- accuracy/run.sh
+++ accuracy/run.sh
@@ -9,20 +9,20 @@ go build -o main
 
 # configure experiment according to Datar, Mayur, et al. 
 # "Locality-sensitive hashing scheme based on p-stable distributions.
-DatasetSize=1000
+DatasetSize=10000 # this only impacts the number of false-positives with high probability 
 NumFeatures=100
-DataMin=-50
-DataMax=50
-NumQueries=10000
+DataMin=-255
+DataMax=255
+NumQueries=100000
 NumProjections=10
-ProjectionWidth=200
+ProjectionWidth=500
 ApproximationFactor=2.0
 NumTables=(5 10 20 30)
 NumNNPerQuery=1
 MaxDistanceToNN=100
 
 # how far a user profile can be from the website context 
-MaxDistanceFromContext=(250 275 300 325 350 375 400) 
+MaxDistanceFromContext=(50 75 100 150 200 250) 
 
 SaveFileName="exp_contextual_vs_targeted.json"
 
